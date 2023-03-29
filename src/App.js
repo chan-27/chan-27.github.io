3f7e5c353f4866947d0fe6c5bd9f9223ca890243
Sai Chandan Kadarla <85214422+chan-27@users.noreply.github.com>
2023-03-30 00:02:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Together 1
+          Sai Chandan Kadarla
         </a>
       </header>
     </div>
